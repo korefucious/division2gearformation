2e2df4b4d2e85657898d92828fa2e9084d767acc
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-03-12 20:04:13 -0700

    created my first repo added division 2 project into it

---
+++ index.js
@@ -0,0 +1,56 @@
+// Document Object Model
+
+
+function namedWeaponsButton(tableid) {
+    let x = document.getElementById(tableid);
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
+}
+
+function assaultRiflesButton() {
+    let x = document.getElementById("assaultRiflesTable");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
+}
+
+function namedGearButton() {
+    let x = document.getElementById("namedGearTable");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
+}
+
+function brandSetsButton() {
+    let x = document.getElementById("brandSetsTable");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
+}
+
+function chestTalentsButton() {
+    let x = document.getElementById("chestTalentsTable");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
+}
+
+function backpackTalentsButton() {
+    let x = document.getElementById("backpackTalentsTable");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
+}
