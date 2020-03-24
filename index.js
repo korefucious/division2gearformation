e8c71021e0b8e7f533f76b187a4309543c4bbeab
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-03-23 20:25:00 -0700

    made all links workable links

--- index.js
+++ index.js
@@ -1,56 +1,10 @@
 // Document Object Model
+let namedWeapons = [
+    
+    { name: "Manic", type: "Assault Rifle", variant: "AK-47",  talent: "Perfectly Overflowing", Description: "Every 2 reloads from empty increases your base magazine capacity by 100%.", location: "Dark Zone" },
 
-
-function namedWeaponsButton(tableid) {
-    let x = document.getElementById(tableid);
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block"
-    }
-}
-
-function assaultRiflesButton() {
-    let x = document.getElementById("assaultRiflesTable");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block"
-    }
-}
-
-function namedGearButton() {
-    let x = document.getElementById("namedGearTable");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block"
-    }
-}
-
-function brandSetsButton() {
-    let x = document.getElementById("brandSetsTable");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block"
-    }
-}
-
-function chestTalentsButton() {
-    let x = document.getElementById("chestTalentsTable");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block"
-    }
-}
-
-function backpackTalentsButton() {
-    let x = document.getElementById("backpackTalentsTable");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block"
-    }
-}
+    { name: "Invisible Hand", type: "Assault Rifle", variant: "AUG", talent: "Perfect Allegro", Description: "Increase rate of fire by 12%.", location: "Dark Zone" },
+    
+    { name: "Shield Splinterer", type: "Assault Rifle", variant: "F2000", talent: "Perfect Allegro", Description: "Increase rate of fire by 12%.", location: "Dark Zone" },
+  
+]
