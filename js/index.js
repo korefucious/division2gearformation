4eaeadac0be870666dbea8d5a5242009cf70d8a0
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-04-10 14:17:54 -0700

    converting data to JSON

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const namedWeaponButtons = document.getElementsByClassName("namedWeaponButton");
-
-namedWeaponButtons.addEventListener('click', (event) => {
-        const namedWeaponTables = document.getElementsByClassName("namedWeapons");
-        namedWeaponTables.style.display = "block"
-    } //close if statement
-}); //closes namedweaponbutton function
